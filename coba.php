bff0537f9da1dbf703c0c1cab13a4eb371d720d3
Andri Adam <andriadam2792000@gmail.com>
2022-03-03 11:36:26 +0700

    first commit

---
+++ coba.php
@@ -0,0 +1,19 @@
+<?php
+
+class Coba
+{
+}
+
+class makan {
+  $a = "makan";
+
+}
+
+$minum = new makan
+$a = new Coba();
+$b = new Coba();
+$c = new Coba();
+
+var_dump($a);
+var_dump($b);
+var_dump($c);
